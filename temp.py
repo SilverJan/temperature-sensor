3c783a45539cf2a34e973f2a201b287691d3130c
Jan Bissinger <jan.bissinger@outlook.de>
2019-01-08 00:15:48 +0800

    Add CSV handling

--- temp.py
+++ temp.py
@@ -4,18 +4,27 @@
 import sys
 import Adafruit_DHT as dht
 import time
+import csv
 
 try:
 	# buffering=0 to overwrite system default buffer
 	# needed when Python runs in background
 	file = open("/home/pi/dev/temperature-sensor/temps.log", "a", buffering=0)
-	while True:
-		timeNow = time.asctime(time.localtime(time.time()))
-		humidity, temperature = dht.read_retry(dht.DHT11, 4)
-		output = "{0} | Temp: {1:0.1f} Humidity: {2:0.1f}".format(timeNow, temperature, humidity)
-		#print output
-		file.write(output + "\n")
-		time.sleep(60)
+	with open("/home/pi/dev/temperature-sensor/test.csv", mode='a') as csv_file:
+		while True:
+			timeNow = time.strftime("%Y-%m-%d %H:%M:%S")
+			humidity, temperature = dht.read_retry(dht.DHT11, 4)
+			if isinstance(humidity, str) or isinstance(temperature, str):
+				print "Error while reading sensor data"
+				continue;
+			output = "{0} | T: {1:0.1f} H: {2:0.1f}".format(timeNow, temperature, humidity)
+			output_arr = [timeNow, temperature, humidity]
+			file.write(output + "\n")
+			
+			csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_NONE)
+			csv_writer.writerows([output_arr])
+			
+			time.sleep(60)
 
 # If keyboard Interrupt is pressed
 except KeyboardInterrupt:
