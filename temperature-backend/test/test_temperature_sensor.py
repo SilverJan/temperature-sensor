cf1c8869e3d2d130260c895c32cf22ee59a46bb3
Jan Bissinger (CI/CBW2) <jan.bissinger@sg.bosch.com>
2020-09-21 11:48:35 +0800

    Update tests

--- temperature-backend/test/test_temperature_sensor.py
+++ temperature-backend/test/test_temperature_sensor.py
@@ -3,8 +3,10 @@
 #   python3 -m pytest -k 'test_run_shell_cmd' -v test/test_temperature_sensor.py
 
 import pytest
+import os
 from test_utils.io import *
 from test_utils.systemd import *
+from test_utils.shell import *
 
 temperature_gather_service = "temperature-gather.service"
 temperature_upload_service = "temperature-upload.service"
@@ -23,6 +25,12 @@ def test_temperature_upload_service_enabled():
 def test_temperature_upload_timer_enabled():
     assert is_service_enabled(temperature_upload_timer)
 
+def test_temperature_upload_service_does_not_fail():
+    (rc, output) = run_in_shell(f"systemctl status {temperature_upload_service}")
+    assert rc == 0
+    assert "successfully uploaded data to firebase" in output
+    assert "Data persisted successfully" in output
+
 @pytest.mark.parametrize(
     'directory', ["/opt/temperature", "/var/log/temperature"])
 def test_directories_exist(directory):
@@ -34,3 +42,4 @@ def test_directories_exist(directory):
              "/var/log/temperature/temps.csv", "/var/log/temperature/temps_big.csv", "/var/log/temperature/temps.json"])
 def test_files_exist(file):
     assert is_file(file)
+    assert os.path.getsize(file) > 0
